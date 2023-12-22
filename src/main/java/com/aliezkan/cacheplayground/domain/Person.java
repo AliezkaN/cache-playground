4e63ba5138e7aed03daca98d7cf0b36fb3703631
Oleh Nahorniak <nagornyak68@gmail.com>
2023-12-23 00:59:24 +0200

    create Person domain

---
+++ src/main/java/com/aliezkan/cacheplayground/domain/Person.java
@@ -0,0 +1,21 @@
+package com.aliezkan.cacheplayground.domain;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String occupation;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String socialSecurityNumber;
+    private String favoriteColor;
+    private String bloodType;
+    private String favoriteBook;
+    private String favoriteMovie;
+    private String zodiacSign;
+}
